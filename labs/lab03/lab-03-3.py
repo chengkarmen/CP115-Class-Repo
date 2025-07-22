5ce3601d8103e86372d43c5a63b95ed0dea1438e
chengkarmen <karmencheng6@gmail.com>
2025-07-22 12:12:30 +0800

    lab 3-3

---
+++ labs/lab03/lab-03-3.py
@@ -0,0 +1,9 @@
+kilobyte = float(input())
+megabyte = kilobyte * 0.001
+gigabyte = kilobyte * 1E-06.0
+terabyte = kilobyte * 1E-09.0
+petabyte = kilobyte * 1E-12.0
+print(megabyte)
+print(gigabyte)
+print(terabyte)
+print(petabyte)
