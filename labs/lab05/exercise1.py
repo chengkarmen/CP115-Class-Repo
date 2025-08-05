6fd34d08a5a258519c768d1946c9643ee3ad346b
chengkarmen <karmencheng6@gmail.com>
2025-08-05 12:22:01 +0800

    exercise 2

--- labs/lab05/exercise1.py
+++ labs/lab05/exercise1.py
@@ -1,7 +1,6 @@
 student_name = input("Enter your name: ")
 course_name = input("Enter the course name: ")
 age = int("Enter your age: ")
-
 print("Student: " + student_name)
 print("Course: " + course_name)
 print("Age: " + age)
