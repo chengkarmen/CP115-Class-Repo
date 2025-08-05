fb5ca9a7085dfb7ecfc8f9419e66ef8b5ace4fbe
chengkarmen <karmencheng6@gmail.com>
2025-08-05 12:46:13 +0800

    exercise 3

---
+++ labs/lab05/exercise3.py
@@ -0,0 +1,9 @@
+import random
+class = input("class name")
+
+from random import randint
+
+student_number = randint(range (1,30))
+
+print("class: " + class)
+print("student number: " + student_number)
