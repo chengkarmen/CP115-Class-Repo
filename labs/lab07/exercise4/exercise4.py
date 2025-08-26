eae171f37b0e6c1e6b98539268ecc15bdcf8a0b4
chengkarmen <karmencheng6@gmail.com>
2025-08-26 11:17:23 +0800

    exercise4

--- labs/lab07/exercise4/exercise4.py
+++ labs/lab07/exercise4/exercise4.py
@@ -2,4 +2,5 @@ import menu_data
 
 print(f"Welcome to {menu_data.restaurant_name}!")
 print(f"Customer Number: {menu_data.customer_num}")
-print(f"")
+print(f"Menu: {menu_data.menu1}\n{menu_data.menu2}\n{menu_data.menu3}")
+print(f"Today's special: {menu_data.special} ")
